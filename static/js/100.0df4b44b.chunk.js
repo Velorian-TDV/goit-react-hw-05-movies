37c70d49c0ee8f06141d468f74fc631928903775
Velorian <TerleckyDima92@gmail.com>
2023-08-20 21:02:41 +0300

    Updates

---
+++ static/js/100.0df4b44b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[100],{100:function(e,a,n){n.r(a),n.d(a,{default:function(){return t}});var s=n(87),i=n(184);function t(){return(0,i.jsxs)("nav",{className:"navigator",children:[(0,i.jsx)(s.OL,{className:"navigator_link",to:"/",children:"Home"}),(0,i.jsx)(s.OL,{className:"navigator_link",to:"/movies",children:"Movies"})]})}}}]);
+//# sourceMappingURL=100.0df4b44b.chunk.js.map
