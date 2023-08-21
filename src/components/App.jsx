ea17f181478736dfe0303416a57be814d8ce9675
Velorian <TerleckyDima92@gmail.com>
2023-08-21 19:11:35 +0300

    bug fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
             <Suspense fallback={<div>Loading ...</div>}>
                 <Routes>
                     <Route path="/" element={<HomePage />} />
-                    <Route path="*" element={<Navigate to='/' />} />
+                    {/* <Route path="*" element={<Navigate to='/' />} /> */}
                     <Route path="/movies" element={<Movies />} />
                     <Route path="/movies/:movieId" element={<Movie />}>
                         <Route path="cast" element={<Cast />} />
