2096f32cbc8ea09f6586d44e1b1beb6082219c24
Velorian <TerleckyDima92@gmail.com>
2023-08-21 19:03:02 +0300

    bug fix

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 # goit-react-hw-05-movies
 ### GitHub Pages: https://velorian-tdv.github.io/goit-react-hw-05-movies/
-qwe';,lqwe[pkqwe[pkqweqwe]]
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,21 +9,21 @@ const Cast = lazy(() => import("./Cast/Cast"));
 const Reviews = lazy(() => import("./Reviews/Reviews"));
 const Movies = lazy(() => import("./Pages/Movies/Movies"));
 
-export default function App() {
-    return (
-        <Wrapper>
-            <Navbar />
-            <Suspense fallback={<div>Loading ...</div>}>
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="*" element={<Navigate to='/' />} />
-                    <Route path="/movies" element={<Movies />} />
-                    <Route path="/movies/:movieId" element={<Movie />}>
-                        <Route path="cast" element={<Cast />} />
-                        <Route path="reviews" element={<Reviews />} />
-                    </Route>
-                </Routes>
-            </Suspense>
-        </Wrapper>
-    )
-}
+// export default function App() {
+//     return (
+//         <Wrapper>
+//             <Navbar />
+//             <Suspense fallback={<div>Loading ...</div>}>
+//                 <Routes>
+//                     <Route path="/" element={<HomePage />} />
+//                     <Route path="*" element={<Navigate to='/' />} />
+//                     <Route path="/movies" element={<Movies />} />
+//                     <Route path="/movies/:movieId" element={<Movie />}>
+//                         <Route path="cast" element={<Cast />} />
+//                         <Route path="reviews" element={<Reviews />} />
+//                     </Route>
+//                 </Routes>
+//             </Suspense>
+//         </Wrapper>
+//     )
+// }
